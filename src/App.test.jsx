7651128773094d7d70ab0d569fdca7dc5c0d546d
perintyler <perintyler@gmail.com>
2023-01-31 00:09:44 -0500

    add banner/title test to `App.test.jsx`

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,2 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders banner with website title', () => {
+    render(<App />);
+    const banner = screen.getByRole('banner');
+    expect(banner).toBeInTheDocument();
+    expect(banner).toHaveTextContent('Poetry File Parser');
+});
+
+
