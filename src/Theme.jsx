47661c0dc2b8bb824f1399cacb7357b7bee50d1f
perintyler <perintyler@gmail.com>
2023-01-30 22:58:09 -0500

    define a materialUI theme in a new file named `Theme.jsx`

---
+++ src/Theme.jsx
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        secondary: {
+            dark:         "#A663CC",
+            main:         "#F26518",
+            light:        "#FCDDF2",
+            contrastText: "#FFFFFF"
+        }
+    }
+});
+
