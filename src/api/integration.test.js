0a65178d504d0b92e382d98bc2a5782745101276
perintyler <perintyler@gmail.com>
2023-02-01 16:11:58 -0500

    trivial integration testing

---
+++ src/api/integration.test.js
@@ -0,0 +1,20 @@
+/** 
+ *   ParsePoetry.js
+ *   --------------
+ * 
+ * Mimimalistic integration tests for poetry file parsing.
+ **/
+
+const fs   = require('fs');   // for synchronous file reading
+const path = require('path'); // for test file path construction
+
+import { getPackages } from './ParsePoetry.js';
+
+test('trivial integration test', () => {
+    for (var testNumber = 0; testNumber < 10; testNumber++) {
+        const testFileName = `integration-test-${testNumber}.lock`;
+        const pathToTestFile = path.join('test-data', testFileName);
+        const poetryFileContents = fs.readFileSync(pathToTestFile, "utf-8");
+        expect(getPackages(poetryFileContents).length).toBeGreaterThan(1);
+    }
+});
