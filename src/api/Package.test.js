e4e74ddf36e81cd9f850c4651edb5e2b75665436
perintyler <perintyler@gmail.com>
2023-02-01 16:11:49 -0500

    test getting package by name

--- src/api/Package.test.js
+++ src/api/Package.test.js
@@ -1,33 +1,44 @@
-/** 
- *   Package.test.js
- *   ---------------
- * 
- * This file tests the `Package` class.
- **/
+/* Package.test.js */
 
 import Package from './Package.js';
 
 test('test `Package` construction', () => {
-    const pkg = new Package(
-        'My Package', 'a package description', 
-        ['first-required-dependency', 'second-required-dependecy'],
-        ['an-optional-dependency']
-    );
+    const dependencyNames = ['first-required-dependency', 'second-required-dependecy'];
+    const optionalDependencyNames = ['an-optional-dependency']
+    const pkg = new Package('My Package', 'a package description', false, dependencyNames, optionalDependencyNames);
 
     expect(pkg.name).toBe('My Package');
     expect(pkg.description).toBe('a package description');
-    expect(pkg.dependency_names.length).toBe(2);
-    expect(pkg.dependency_names[0]).toBe('first-required-dependency');
-    expect(pkg.dependency_names[1]).toBe('second-required-dependecy');
-    expect(pkg.optional_dependency_names.length).toBe(1);
-    expect(pkg.optional_dependency_names[0]).toBe('an-optional-dependency');
+    expect(pkg.dependencyNames.length).toBe(2);
+    expect(pkg.dependencyNames[0]).toBe('first-required-dependency');
+    expect(pkg.dependencyNames[1]).toBe('second-required-dependecy');
+    expect(pkg.optionalDependencyNames.length).toBe(1);
+    expect(pkg.optionalDependencyNames[0]).toBe('an-optional-dependency');
 });
 
-test('test `Package` construction with default constructors', () => {
-    const pkg = new Package('My Package', 'a package description');
-    
-    expect(pkg.name).toBe('My Package');
-    expect(pkg.description).toBe('a package description');
-    expect(pkg.dependency_names.length).toBe(0);
-    expect(pkg.optional_dependency_names.length).toBe(0);
+test('test static class member containing every constructed package', () => {
+    if (Package.all) { Package.all = {} } // clear packages from already ran tests
+
+    const pkg1 = new Package('Package 1', 'package 1 description', false, [], []);
+    const pkg2 = new Package('Package 2', 'package 2 description', false, [], []);
+    const pkg3 = new Package('Package 3', 'package 3 description', false, [], []);
+    const pkg4 = new Package('Package 4', 'package 4 description', false, [], []);
+    const pkg5 = new Package('Package 5', 'package 5 description', false, [], []);
+
+    expect(Object.keys(Package.all).length).toBe(5);
+
+    [1, 2, 3, 4, 5].forEach((packageNumber) => {
+        const packageName = 'Package ' + packageNumber;
+        expect(Object.hasOwn(Package.all, packageName));
+        let pkg = Package.all[packageName];
+        expect(pkg.name).toBe(packageName);
+        expect(pkg.description).toBe('package ' + packageNumber + ' description');
+    });
 });
+
+test('test Package::getDependencies', () => {
+});
+
+test('test Package::getReverseDepencies', () => {
+});
+
