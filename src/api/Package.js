0171c4d6763a692989ed37bdbf6629e3a8263c7d
perintyler <perintyler@gmail.com>
2023-02-01 16:11:38 -0500

    function for getting package by its name

--- src/api/Package.js
+++ src/api/Package.js
@@ -7,20 +7,35 @@
 
 export default class Package 
 {
+    static all = {}; // maps package name to package object
+
     constructor(
       name,
       description,
-      dependency_names = [],
-      optional_dependency_names = [],
+      isOptional,
+      dependencyNames,
+      optionalDependencyNames
     ) {
         this.name = name;
         this.description = description;
-        this.dependency_names = dependency_names;
-        this.optional_dependency_names = optional_dependency_names;
+        this.isOptional = isOptional;
+        this.dependencyNames = dependencyNames ? dependencyNames : [];
+        this.optionalDependencyNames = optionalDependencyNames ? optionalDependencyNames : [];
+        this.reverseDependencyNames = [];
+
+        Package.all[this.name] = this;
     }
 
     toString() 
     {
         return this.name;
     }
+
+    static getByName(packageName)
+    {
+        return packageName in Package.all
+             ? Package.all[packageName]
+             : null;
+    }
 }
+
