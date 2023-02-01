90c4265aec28998dbfee78acbae51193ef8e1163
perintyler <perintyler@gmail.com>
2023-02-01 16:10:39 -0500

    test poetry file parsing

--- src/api/ParsePoetry.test.js
+++ src/api/ParsePoetry.test.js
@@ -1,19 +1,14 @@
-/** 
- *   ParsePoetry.test.js
- *   -------------------
- * 
- * This file tests the `getPackages` function from `ParsePoetry.js`.
- **/
+/* ParsePoetry.test.js */
 
 const fs   = require('fs');   // for synchronous file reading
 const path = require('path'); // for test file path construction
 
-import getPackages from './ParsePoetry.js';
+import { getPackages } from './ParsePoetry.js';
 
-/* This function, which wraps the `getPackages` function, takes the name of a 
- * Poetry lockfile found in the `test-data` directory (located in the root folder 
- * of the git repository), reads/parses it, and returns an array of `Package` objects. 
- */
+// This function (which wraps the `getPackages` function) takes the name of a 
+// Poetry lockfile from the `test-data` directory (located in the root folder 
+// of the git repository), reads/parses it, and returns an array of `Package` 
+// objects.
 function getPackagesFromTestFile(testFileName)
 {
     const pathToTestFile = path.join('test-data', testFileName + '.lock');
@@ -28,18 +23,18 @@ test('test `getPackages` on poetry file with no package dependencies', () =>
 
     expect(packages[0].name).toBe('atomicwrites');
     expect(packages[0].description).toBe('Atomic file writes.');
-    expect(packages[0].dependency_names.length).toBe(0);
-    expect(packages[0].optional_dependency_names.length).toBe(0);
+    expect(packages[0].dependencyNames.length).toBe(0);
+    expect(packages[0].optionalDependencyNames.length).toBe(0);
 
     expect(packages[1].name).toBe('attrs');
     expect(packages[1].description).toBe('Classes Without Boilerplate');
-    expect(packages[1].dependency_names.length).toBe(0);
-    expect(packages[1].optional_dependency_names.length).toBe(0);
+    expect(packages[1].dependencyNames.length).toBe(0);
+    expect(packages[1].optionalDependencyNames.length).toBe(0);
 
     expect(packages[2].name).toBe('cachecontrol');
     expect(packages[2].description).toBe('httplib2 caching for requests');
-    expect(packages[2].dependency_names.length).toBe(0);
-    expect(packages[2].optional_dependency_names.length).toBe(0);
+    expect(packages[2].dependencyNames.length).toBe(0);
+    expect(packages[2].optionalDependencyNames.length).toBe(0);
 });
 
 test('test `getPackages` on poetry file with a single package', () => 
@@ -50,18 +45,26 @@ test('test `getPackages` on poetry file with a single package', () =>
     expect(packages[0].name === 'cachecontrol');
     expect(packages[0].description === 'httplib2 caching for requests');
 
-    expect(packages[0].dependency_names.length === 3);
-    expect(packages[0].dependency_names[0] === 'lockfile');
-    expect(packages[0].dependency_names[1] === 'msgpack');
-    expect(packages[0].dependency_names[2] === 'requests');
+    expect(packages[0].dependencyNames.length === 3);
+    expect(packages[0].dependencyNames[0] === 'lockfile');
+    expect(packages[0].dependencyNames[1] === 'msgpack');
+    expect(packages[0].dependencyNames[2] === 'requests');
 
-    expect(packages[0].optional_dependency_names.length === 2);
-    expect(packages[0].optional_dependency_names[0] === 'filecache');
-    expect(packages[0].optional_dependency_names[1] === 'redis');
+    expect(packages[0].optionalDependencyNames.length === 2);
+    expect(packages[0].optionalDependencyNames[0] === 'filecache');
+    expect(packages[0].optionalDependencyNames[1] === 'redis');
 });
 
 test('test `getPackages` on the poetry file used by the Poetry package', () => 
 {
     const packages = getPackagesFromTestFile('lockfile-used-for-poetry-package');
     expect(packages.length === 70);
+});
+
+test('test optional dependencies', () => 
+{
+    const packages = getPackagesFromTestFile('lockfile-with-no-package-dependencies');
+    expect(packages[0].isOptional).toBe(false);
+    expect(packages[1].isOptional).toBe(false);
+    expect(packages[2].isOptional).toBe(true);
 });
